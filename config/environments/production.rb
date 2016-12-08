57ac97ec2f49d15d0d65c44a28761be468f0a17a
Ambre Palanque <ambre.palanque@hotmail.com>
2016-12-08 22:10:31 +0100

    Adds a missing '}'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,8 @@ Rails.application.configure do
             secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
             s3_region: ENV.fetch('AWS_REGION')
         }
+    }
 
     config.consider_all_requests_local = true
-    
+
 end
